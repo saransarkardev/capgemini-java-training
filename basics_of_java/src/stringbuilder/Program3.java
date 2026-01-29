022266b751b92933e826356869dd910c361b5084
saransarkardev <saran.sarkar.dev@gmail.com>
2026-01-29 22:41:16 +0530

    Fixed some codes on String Builder

--- basics_of_java/src/stringbuilder/Program3.java
+++ basics_of_java/src/stringbuilder/Program3.java
@@ -14,11 +14,15 @@ public class Program3 {
 		System.out.println(sb);
 		
 		// start deleting the characters from index 3 of sb upto 5 , index-1.
+		
 		sb.delete(3,6);
+		
 		System.out.println(sb);
 		
 		// insert the string value at index 2 of the string
+		
 		sb.insert(2,"student");
+		
 		System.out.println(sb);
 				
 	}
