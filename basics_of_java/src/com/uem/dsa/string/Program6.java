655891916ea3377356cd91f650dcab531420e895
saransarkardev <saran.sarkar.dev@gmail.com>
2026-01-30 20:45:51 +0530

    Added a string code

--- basics_of_java/src/com/uem/dsa/string/Program6.java
+++ basics_of_java/src/com/uem/dsa/string/Program6.java
@@ -1,7 +1,12 @@
 package com.uem.dsa.string;
 
+// Convert string to uppercase and lowercase.
 public class Program6 {
 	public static void main(String[] args) {
 		
+		String s = "Saran";
+		
+		System.out.println("UpperCase: " + s.toUpperCase());
+		System.out.println("LowerCase: " + s.toLowerCase());
 	}
 }
