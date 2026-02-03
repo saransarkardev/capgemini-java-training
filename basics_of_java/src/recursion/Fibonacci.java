e7b2cbc90688beef27ca0735b493a0ab42065369
saransarkardev <saran.sarkar.dev@gmail.com>
2026-02-03 21:24:08 +0530

    Added programs on recursion

---
+++ basics_of_java/src/recursion/Fibonacci.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class Fibonacci {
+	static int fib(int n) {
+		
+		if (n==1) return 1;
+		if (n == 0) return 0;
+		
+		return fib(n-1) + fib(n-2);
+		
+	}
+	public static void main(String[] args) {
+		int n = 3;
+		
+		System.out.println(fib(n));
+	}
+}
---
+++ basics_of_java/src/recursion/FibonacciDP.java
@@ -0,0 +1,25 @@
+package recursion;
+
+import java.util.Arrays;
+
+public class FibonacciDP {
+	public static void main(String[] args) {
+		int n = 5;
+		
+		int[] dp = new int[n+1];
+		Arrays.fill(dp, -1);
+		System.out.println(fib(n,dp));
+	}
+	
+	private static int fib(int n, int[] dp) {
+		if (n <= 1) 
+			return n;
+		
+		if (dp[n] != -1) {
+			return dp[n];
+		}
+		;
+		dp[n] = fib(n-1, dp) + fib(n-2, dp);
+		return dp[n];
+ 	}
+}
